39ea1f09fce4fd3f3101a951207505cf621fcf79
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-05-29 14:46:08 -0400

    fix: alterado para usar dropdown na table

--- resources/views/livewire/views/financas/bancos/listagem.blade.php
+++ resources/views/livewire/views/financas/bancos/listagem.blade.php
@@ -1,22 +1,22 @@
 @php
-  $tiposBancos = [
-    ['tipo' => 'corrente', 'label' => 'Conta Corrente'],
-    ['tipo' => 'poupanca', 'label' => 'Conta Poupança'],
-    ['tipo' => 'carteira', 'label' => 'Carteira Física'],
-    ['tipo' => 'digital', 'label' => 'Conta Digital'],
-    ['tipo' => 'investimento', 'label' => 'Investimentos'],
-    ['tipo' => 'caixa_empresa', 'label' => 'Caixa da Empresa'],
-    ['tipo' => 'cartao_credito', 'label' => 'Cartão de Crédito'],
-    ['tipo' => 'moeda_estrangeira', 'label' => 'Conta em Moeda Estrangeira'],
-    ['tipo' => 'outro', 'label' => 'Outro'],
-  ];
+$tiposBancos = [
+['tipo' => 'corrente', 'label' => 'Conta Corrente'],
+['tipo' => 'poupanca', 'label' => 'Conta Poupança'],
+['tipo' => 'carteira', 'label' => 'Carteira Física'],
+['tipo' => 'digital', 'label' => 'Conta Digital'],
+['tipo' => 'investimento', 'label' => 'Investimentos'],
+['tipo' => 'caixa_empresa', 'label' => 'Caixa da Empresa'],
+['tipo' => 'cartao_credito', 'label' => 'Cartão de Crédito'],
+['tipo' => 'moeda_estrangeira', 'label' => 'Conta em Moeda Estrangeira'],
+['tipo' => 'outro', 'label' => 'Outro'],
+];
 
-  $headers = [
-    ['key' => 'id', 'label' => '#'],
-    ['key' => 'nome', 'label' => 'Nome'],
-    ['key' => 'tipo', 'label' => 'Tipo'],
-    ['key' => 'saldo_atual', 'label' => 'Saldo atual'],
-  ];
+$headers = [
+['key' => 'id', 'label' => '#'],
+['key' => 'nome', 'label' => 'Nome'],
+['key' => 'tipo', 'label' => 'Tipo'],
+['key' => 'saldo_atual', 'label' => 'Saldo atual'],
+];
 
 @endphp
 <div class="p-4 md:p-6">
@@ -27,10 +27,10 @@
       <x-input wire:model.live.debounce="pesquisa" label="Pesquisa" placeholder="Nome, tipo, agência, conta..." icon="o-magnifying-glass" inline />
     </div>
     <div class="flex-2">
-      <x-select label="Tipo do banco" wire:model.live.debounce="pesquisaTipoBanco" :options="$tiposBancos" option-value="tipo" inline option-label="label" placeholder="Selecione o tipo do banco..."/>
+      <x-select label="Tipo do banco" wire:model.live.debounce="pesquisaTipoBanco" :options="$tiposBancos" option-value="tipo" inline option-label="label" placeholder="Selecione o tipo do banco..." />
     </div>
     <div class="flex-1">
-      <x-toggle id="ativo" label="Ativo?" wire:model.live.debounce="consultaAtivo"/>
+      <x-toggle id="ativo" label="Ativo?" wire:model.live.debounce="consultaAtivo" />
     </div>
     <div class="flex-1">
       <x-button label="Cadastrar" icon="o-plus" class="btn btn-success w-full md:w-auto" link="{{ route('financas.bancos.cadastro') }}" />
@@ -43,32 +43,11 @@
     @endscope
 
     @scope('actions', $banco)
-    <div class="flex flex-row">
-      <x-popover>
-        <x-slot:trigger>
-          <x-button icon="o-eye" wire:click="setVisualizacaoBanco({{ $banco->id }})" />
-        </x-slot:trigger>
-        <x-slot:content>
-          Visualizar
-        </x-slot:content>
-      </x-popover>
-      <x-popover>
-        <x-slot:trigger>
-          <x-button icon="o-pencil-square" link="{{ route('financas.bancos.edicao', ['id' => $banco->id]) }}"/>
-        </x-slot:trigger>
-        <x-slot:content>
-          Editar
-        </x-slot:content>
-      </x-popover>
-      <x-popover>
-        <x-slot:trigger>
-          <x-button icon="o-eye-slash" wire:click="setInativacaoBanco({{ $banco->id }})"/>
-        </x-slot:trigger>
-        <x-slot:content>
-          Inativar
-        </x-slot:content>
-      </x-popover>
-    </div>
+    <x-dropdown>
+      <x-menu-item title="Visualizar" icon="o-eye" wire:click="setVisualizacaoBanco({{ $banco->id }})" />
+      <x-menu-item title="Editar" icon="o-pencil-square" link="{{ route('financas.bancos.edicao', ['id' => $banco->id]) }}" />
+      <x-menu-item title="{{ !$banco->trashed() ? 'Inativar' : 'Restaurar' }}" icon="o-arrow-path-rounded-square" wire:click="setInativacaoBanco({{ $banco->id }})" />
+    </x-dropdown>
     @endscope
   </x-table>
 
@@ -118,12 +97,12 @@
 
   <x-modal wire:model="modalInativacao" title="Inativação do banco" class="backdrop-blur" box-class="max-w-xl w-11/12">
     @if ($bancoAtual !== null)
-      <p class="font-semibold text-lg">Você pretende realmente inativar esse banco? Todas as movimentações em relatórios não serão contabilizados e nenhuma movimentação não poderá ser adicinado no banco inativo.</p>
+    <p class="font-semibold text-lg">Você pretende realmente inativar esse banco? Todas as movimentações em relatórios não serão contabilizados e nenhuma movimentação não poderá ser adicinado no banco inativo.</p>
 
-      <x-slot:actions>
-        <x-button label="Cancelar" class="btn btn-primary" @click="$wire.set('modalInativacao', false)"/>
-        <x-button label="Inativar" class="btn btn-error" wire:click="inativarBanco" wire:loading.attr="disabled" spinner="inativarBanco"/>
-      </x-slot:actions>
+    <x-slot:actions>
+      <x-button label="Cancelar" class="btn btn-primary" @click="$wire.set('modalInativacao', false)" />
+      <x-button label="Inativar" class="btn btn-error" wire:click="inativarBanco" wire:loading.attr="disabled" spinner="inativarBanco" />
+    </x-slot:actions>
     @endif
   </x-modal>
 
