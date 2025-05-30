874e2c985cd8886a270822f3fd913ed2f17e6834
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-05-30 16:37:08 -0400

    feat: finalizado rotina de listagem de receitas

--- app/Livewire/Views/Financas/Receitas/Listagem.php
+++ app/Livewire/Views/Financas/Receitas/Listagem.php
@@ -2,16 +2,74 @@
 
 namespace App\Livewire\Views\Financas\Receitas;
 
+use App\Models\Receita;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Listagem extends Component
 {
+  use WithPagination;
+
+  public ?string $pesquisa = null;
+  public ?string $data_recebimento = null;
+  public int $porPagina = 10;
+  public Authenticatable|User $usuario;
+
+  public function mount(): void
+  {
+    $this->usuario = Auth::user();
+  }
+
   #[Title('Finanças - Receitas')]
   #[Layout('components.layouts.autenticado')]
   public function render()
   {
-    return view('livewire.views.financas.receitas.listagem');
+    return view('livewire.views.financas.receitas.listagem', [
+      'receitas' => $this->receitas(),
+    ]);
+  }
+
+  public function updated(string $props, mixed $valor): void
+  {
+    if (in_array($props, ['pesquisa', 'data_recebimento'])) {
+      $this->receitas();
+    }
+  }
+
+  public function resetaFiltros(): void {
+    $this->reset('pesquisa');
+    $this->reset('data_recebimento');
+  }
+
+  public function receitas(): LengthAwarePaginator
+  {
+    $query = Receita::query()->where('user_id', $this->usuario->id)->with('banco')->with('categoria');
+
+    $query->where(function ($q) {
+      $q->where('descricao', 'like', "%{$this->pesquisa}%")
+        ->orWhere('observacao', 'like', "%{$this->pesquisa}%")
+        ->orWhere('valor', $this->pesquisa);
+    });
+
+    if ($this->data_recebimento) {
+      $datas = str_contains($this->data_recebimento, ' até ')
+        ? explode(' até ', $this->data_recebimento)
+        : $this->data_recebimento;
+
+      if (is_array($datas)) {
+        $query->whereBetween('data', array_map(fn($d) => Carbon::parse($d)->startOfDay(), $datas));
+      } else {
+        $query->whereDate('data', Carbon::parse($datas)->toDateString());
+      }
+    }
+
+    return $query->paginate($this->porPagina);
   }
 }
--- resources/views/livewire/views/financas/receitas/listagem.blade.php
+++ resources/views/livewire/views/financas/receitas/listagem.blade.php
@@ -1,18 +1,59 @@
 @php
   $configuracaoDatetime = ['altFormat' => 'd/m/Y', 'mode' => 'range'];
+
+  $headers = [
+    ['key' => 'id', 'label' => '#'],
+    ['key' => 'descricao', 'label' => 'Descrição'],
+    ['key' => 'valor', 'label' => 'Valor'],
+    ['key' => 'data', 'label' => 'Data da movimentação'],
+    ['key' => 'banco.nome', 'label' => 'Banco'],
+    ['key' => 'categoria.nome', 'label' => 'Categoria'],
+    ['key' => 'recorrente', 'label' => 'Recorrente?'],
+  ];
 @endphp
 <div class="p-4 md:p-6">
   <h1 class="text-2xl font-bold mb-6">Receitas</h1>
 
   <div class="flex flex-col md:flex-row md:items-end gap-4 mb-8">
     <div class="flex-3">
-      <x-input wire:model="pesquisa" label="Pesquisa" placeholder="Nome, descrição, observação..." inline/>
+      <x-input wire:model.live.debounce="pesquisa" label="Pesquisa" placeholder="Nome, descrição, observação..." inline/>
     </div>
     <div class="flex-1">
-      <x-datepicker label="Recebimento" wire:model="data_recebimento" icon="o-calendar" :config="$configuracaoDatetime" inline/>
+      <x-datepicker label="Recebimento" wire:model.change="data_recebimento" icon="o-calendar" :config="$configuracaoDatetime" inline/>
     </div>
-    <div class="flex-1">
+    <div class="flex flex-col md:flex-row gap-2 flex-1">
+      <x-button label="Resetar filtros" icon="o-funnel" wire:click="resetaFiltros" class="btn btn-primary"/>
       <x-button label="Cadastrar" icon="o-plus" link="{{route('financas.receitas.cadastro')}}" class="btn btn-success"/>
     </div>
   </div>
+
+  <x-table :rows="$receitas" :headers="$headers" with-pagination>
+    @scope('cell_valor', $receita)
+      R$ {{ number_format($receita->valor, 2, ',', '.') }}
+    @endscope
+
+    @scope('cell_recorrente', $receita)
+      @if ($receita->recorrente)
+        @if ($receita->receita_pai_id)
+          Parcela filha
+        @else
+          Parcela pai
+        @endif
+      @else
+        Não
+      @endif
+    @endscope
+
+    @scope('cell_data', $receita)
+      {{ Carbon\Carbon::parse($receita->data)->format('d/m/Y') }}
+    @endscope
+
+    @scope('actions', $receita)
+      <x-dropdown>
+        <x-menu-item title="Visualizar" icon="o-eye" />
+        <x-menu-item title="Editar" icon="o-pencil-square" />
+        <x-menu-item title="Deletar" icon="o-trash" />
+      </x-dropdown>
+    @endscope
+  </x-table>
 </div>
